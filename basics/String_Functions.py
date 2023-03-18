f71ab40a17aa14b2d189fc8a5088da5b8354b9fd
Andres Licona <andreslicona27@icloud.com>
2023-03-18 19:17:34 +0100

    advances of day three

---
+++ basics/String_Functions.py
@@ -0,0 +1,26 @@
+capitalize() # converts first letter in a capital letter 
+lower() # lower case all the letters
+upper() # upper case all the letters
+count() # count the amount of times a character repeats in a string, Case sensitive
+find() # return the index of the suffix you add 
+center() # centers the string in the space you give in the first parameter with the character you give in the second parameter 
+
+isalnum() # is alphanumeric character
+isalpha() # checks if all characters are alphanumeric 
+isdecimal() # checks if its a decimal number 
+isdigit() # check if its a digit
+isidentifier() # checks if its a valid identifier 
+islower() # checks if all character are lowercase 
+isnumeric() # check if its numeric 
+isprintable() # checks if its a printable character 
+isspace() # checks if its a space 
+istitle() # Checks for Titlecased String
+isupper() # returns if all characters are uppercase characters
+
+join() # return a concatenated string 
+replace() # replaces substring inside
+rfind() # return the highest index of substring 
+startswith() # Checks if String Starts with the Specified String
+strip() # Removes Both Leading and Trailing Characters
+swapcase() # swap uppercase characters to lowercase; vice versa
+title() # Returns a Title Cased String
--- basics/basics.py
+++ basics/basics.py
@@ -1,6 +1,8 @@
-#You have to use '' to print a message with quotation marks
+
+# ! DAY 1
+# You have to use '' to print a message with quotation marks
 print("hello" + " " + "'Andres'")
-#It would ask the input before showing the complete message 
+# It would ask the input before showing the complete message 
 name = input("What is your name?")
 length = len(name) #To know the amount of characters of a string 
 
@@ -8,36 +10,64 @@ print("Hello " + input("Write your name? ") + "!")
 
 print(length)
 
-#? PROJECT
-print("Welcome to the Band Name Generator")
-cityName = input("What's the name of the city you grew up in?\n")
-petName = input("What's your pet's name?\n")
-print("\nYour band name could be " + cityName + " " + petName)
-
+# ! DAY 2
 #?PRIMITIVE DATA TYPES
 print("hello"[0]) #Prints letter h 
-#123_456_789 you can put "_" in numbers for it to be more easy for you to read them 
+# 123_456_789 you can put "_" in numbers for it to be more easy for you to read them 
 
 num = len(input("Whats your name?"))
-print(type(num)) #Gives you the class of the variable 
+print(type(num)) # Gives you the class of the variable 
 
-new_num = str(num) #Converts the variable in a string
+new_num = str(num) # Converts the variable in a string
 print(type(new_num))
 
 print("your name has " + new_num + " characters")
 
 a = float(100)
 
-#Every time you divide you convert the type numbers to float 
-#Use "**" to get the Exponential number 
+# Every time you divide you convert the type numbers to float 
+# Use "**" to get the Exponential number 
 
 print(round(10 / 3)) #for you to round numbers 
 print(round(10 / 3, 3)) #for you to round numbers with decimal places 
 print(round(10 // 3)) #gives you a integer 
 
-#f-String
+# f-String
 score = 0
 height = 1.8
 isWinning = True
-print(f"your score is {score}, your height is {height}, and your are winning is {isWinning}" )
+print(f"your score is {score}, your height is {height}, and your are winning is {isWinning}")
+
+# ! DAY 3
+# ? Conditional statements help for coding not to overflow 
+# ? if / elif / else and Multiple if
+height = int(input(print("Give me your height in cm:")))
+if height > 120:
+    print("You can ride the rollercoaster")
+    age = int(input(print("Give me your age:")))
+    if age < 12:
+        bill = 5
+        print("Chields pay 5 ")
+    elif age <= 18:
+        bill = 7
+        print("Young people pay 7")
+    elif age >= 35 and age <= 45:
+        bill= 7
+        print("Ride without risk")
+    else:
+        bill = 12
+        print("Adults pay 12")
+
+    photo = input("Do you want a photo? Y / N")
+    if photo == "Y":
+        bill += 3
+    
+    print(f"Your final bill is {bill}")
+else:
+    print("You have to be taller")
+ 
+
+
+
+
 
