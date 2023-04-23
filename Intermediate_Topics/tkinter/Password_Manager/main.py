055dc44abd9ca0844cbf06306b8f192e65b918c9
Andres Licona <andreslicona27@icloud.com>
2023-04-23 14:20:23 +0200

    flash cards app

--- Intermediate_Topics/tkinter/Password_Manager/main.py
+++ Intermediate_Topics/tkinter/Password_Manager/main.py
@@ -70,15 +70,15 @@ def find_password():
             data = json.load(data_file)
 
     except FileNotFoundError:
-        messagebox.show(title="Error", message="No data file found")
+        messagebox.showinfo(title="Error", message="No data file found")
 
     else:
         if website in data:
             email = data[website]["email"]
             password = data[website]["password"]
-            messagebox.show(title=website, message=f"Email: {email}\nPassword: {password}")
+            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
         else:
-            messagebox.show(title=website, message=f"No details for {website} exists")
+            messagebox.showinfo(title=website, message=f"No details for {website} exists")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -93,34 +93,34 @@ canvas.grid(column=1, row=0)
 
 # Labels
 website_label = Label(text="Website:")
-website_label.grid(column=0, row=1)
+website_label.grid(column=0, row=1, pady=10)
 
 email_label = Label(text="Email/Username:")
-email_label.grid(column=0, row=2)
+email_label.grid(column=0, row=2, pady=10)
 
 password_label = Label(text="Password:")
-password_label.grid(column=0, row=3)
+password_label.grid(column=0, row=3, pady=10)
 
 # Entries
 website_entry = Entry(width=21)
-website_entry.grid(column=1, row=1)
+website_entry.grid(column=1, row=1, pady=10)
 website_entry.focus()
 
-email_entry = Entry(width=35)
-email_entry.grid(column=1, row=2, columnspan=2)
+email_entry = Entry(width=40)
+email_entry.grid(column=1, row=2, columnspan=2, pady=10)
 email_entry.insert(0, "andreslicona27@gmail.com")
 
 password_entry = Entry(width=21)
-password_entry.grid(column=1, row=3)
+password_entry.grid(column=1, row=3, pady=10)
 
 # Buttons
 search_button = Button(text="Search", width=13, command=find_password)
-search_button.grid(column=2, row=1)
+search_button.grid(column=2, row=1, pady=10)
 
 generate_password_button = Button(text="Generate Password", command=generate_password)
-generate_password_button.grid(column=2, row=3)
+generate_password_button.grid(column=2, row=3, pady=10)
 
 add_button = Button(text="Add", width=36, command=save)
-add_button.grid(column=1, row=4, columnspan=2)
+add_button.grid(column=1, row=4, columnspan=2, pady=10)
 
 window.mainloop()
