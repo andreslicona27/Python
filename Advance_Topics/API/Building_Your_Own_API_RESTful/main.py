69d6517e198a14b90841f86c1b6b9e3cc7f1ae6d
Andres Licona <andreslicona27@icloud.com>
2023-07-06 13:10:58 +0200

    Capstone project part 3 Authentication with flask

--- Advance_Topics/API/Building_Your_Own_API_RESTful/main.py
+++ Advance_Topics/API/Building_Your_Own_API_RESTful/main.py
@@ -113,5 +113,6 @@ def delete_cafe(cafe_id):
     else:
         return jsonify(error={"Forbidden": "Sorry, that's not allowed. Make sure you have the correct api_key."}), 403
 
+
 if __name__ == '__main__':
     app.run(debug=True)
